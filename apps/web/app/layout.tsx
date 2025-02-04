3829d41d89ace72b7ab8f83abcabe36ab64732c4
reach0908 <wlsghkim0908@gmail.com>
2025-02-04 09:30:03 +0900

    chore(turbo): 터보 버전 업 및 global.css 임포트 안되는 문제 수정

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
-import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
+
+import "@workspace/ui/styles/globals.css";
 
 const fontSans = Geist({
   subsets: ["latin"],
